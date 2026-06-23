cd01e0eacc47f18e580e59969f268f956a9f3bf2
JeanJAKK <apotrekokoujean06@gmail.com>
2026-06-23 15:24:04 +0000

    add ui.js

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,13 +1,50 @@
 import { questions } from "../data/questions.js";
 
-const questionTest = document.querySelector(".question p");
-const rep1 = document.getElementById("btn1");
-const rep2 = document.querySelector("#btn2");
-const rep3 = document.querySelector("#btn3");
-const rep4 = document.querySelector("#btn4");
+const homePage = document.querySelector(".page_accueil");
+const quizPage = document.querySelector(".page_quiz");
+const resultPage = document.querySelector(".page_resultat");
+const startBtn = document.querySelector("#demarrer");
 
-questionTest.textContent = questions[0].question;
-rep1.textContent = questions[0].choix[0];
-rep2.textContent = questions[0].choix[1];
-rep3.textContent = questions[0].choix[2];
-rep4.textContent = questions[0].choix[3];
+// Fonction hideScreen
+function hideScreen(page) {
+  page.style.display = "none";
+}
+
+// Fonction showScreen
+function showScren(page) {
+  page.style.display = "block";
+}
+
+// Game starter
+startBtn.addEventListener("click", () => {
+  hideScreen(homePage);
+  hideScreen(resultPage);
+  showScren(quizPage);
+});
+
+const questionTest = document.querySelector(".question p");
+const response = document.querySelectorAll(".response");
+
+function validatorResponse(questionIndex) {
+  questionTest.textContent = questions[questionIndex].question;
+
+  response.forEach((element, i) => {
+    element.textContent = questions[questionIndex].choix[i];
+    element.addEventListener("click", () => {
+      if (i === questions[questionIndex].reponse) {
+        element.style.background = "#4CAF50";
+        element.style.color = "white";
+      } else {
+        element.style.background = "#F44336";
+        element.style.color = "white";
+      }
+    });
+  });
+}
+
+// display question
+const intervalle = setInterval(() => {
+  let index = Math.floor(Math.random() * questions.length);
+  //window.alert(index);
+  validatorResponse(index);
+}, 30000);
